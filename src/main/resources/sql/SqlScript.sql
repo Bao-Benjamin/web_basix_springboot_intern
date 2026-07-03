e3742f30802a956ba4d9d49e560e71802b10ffed
Bao-Benjamin <117798501+Bao-Benjamin@users.noreply.github.com>
2026-07-03 16:41:42 +0700

    Update SqlScript.sql

--- src/main/resources/sql/SqlScript.sql
+++ src/main/resources/sql/SqlScript.sql
@@ -1,28 +1,22 @@
 CREATE DATABASE plant_management;
 use plant_management;
 create table plant (
-id int auto_increment primary key,
+    id int auto_increment primary key,
     name varchar(50),
     price decimal(12,2),
     plant_date date ,
     img text,
     quantity int,
     description text,
-    created_at timestamp default current_timestamp
+    created_at timestamp default current_timestamp,
+    category_id int ,
+	CONSTRAINT fk_plant_category 
+    FOREIGN KEY (category_id) 
+    REFERENCES category(id) 
+    ON DELETE cascade
     );
 
 create table category(
     id int auto_increment primary key,
     name varchar(50)
 );
-
-create table plant_cate(
-plant_id int, 
-    category_id int,
-    constraint fk_plant
-    foreign key (plant_id) references plant(id) on delete cascade,
-    constraint fk_category
-    foreign key (category_id) references category(id) on delete cascade
-
-) 
-
